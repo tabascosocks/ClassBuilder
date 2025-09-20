288b78c78581958976dbb827af4860574c441ee2
tabascosocks <jak.baddams@gmail.com>
2025-09-20 20:27:06 +0930

    .

--- src/main/java/com/edumentic/Main.java
+++ src/main/java/com/edumentic/Main.java
@@ -6,6 +6,10 @@ package com.edumentic;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import java.util.logging.LogManager;
+import java.io.IOException;
+
+import java.io.InputStream;
 
 @Slf4j
 public class Main {
@@ -14,6 +18,14 @@ public class Main {
     private int myNum;
 
     public static void main(String[] args) {
+        try {
+            // Load logging configuration
+            InputStream inputStream = Main.class.getResourceAsStream("/logging.properties");
+            LogManager.getLogManager().readConfiguration(inputStream);
+        } catch (IOException e) {
+            System.err.println("Error loading logging configuration: " + e.getMessage());
+        }
+
         log.debug("Helloaskd asdlasd ;aslkd l;k asdlask d;las");
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
